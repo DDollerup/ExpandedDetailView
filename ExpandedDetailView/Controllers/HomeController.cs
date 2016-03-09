d10b3e7c803e3ba6a554cd3d20ba28882b647939
PDDaniel <daniel@proper-dog.com>
2016-03-09 13:27:06 +0100

    Added Search

--- ExpandedDetailView/Controllers/HomeController.cs
+++ ExpandedDetailView/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ExpandedDetailView.Controllers
             if (id == 0)
             {
                 List<Product> allProducts = productFac.GetAll();
-                return View(allProducts); 
+                return View(allProducts);
             }
             else
             {
@@ -35,7 +35,7 @@ namespace ExpandedDetailView.Controllers
 
         public ActionResult ShowProduct(int id)
         {
-           
+
             Product productToGet = productFac.GetProduct(id);
             return View(productToGet);
         }
@@ -49,6 +49,31 @@ namespace ExpandedDetailView.Controllers
         public ActionResult ListProductsByCategoryID(int id)
         {
             return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="searchString"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult SearchResult(string searchString)
+        {
+            // Get all products from productFac through the Method GetAll()
+            List<Product> allProducts = productFac.GetAll();
+
+            // Saving the search result in TempData["SearchResult"] which will persist through one POST
+            // We then search the list for products which contains the users' searchString 
+            TempData["SearchResult"] = allProducts.Where(x => x.Name.ToLower().Contains(searchString.ToLower())).ToList();
+
+            // We the redirect the result to the Search View.
+            return RedirectToAction("Search");
         }
     }
 }
